d160e9d7f1d2445d8a8c69cba72b2f625a302eb7
lnoir <gh@latn.black>
2023-12-04 07:08:43 +0000

    feat: day 4, part a

---
+++ src/lib/04/04.a.ts
@@ -0,0 +1,47 @@
+import { loadLinesFromFile } from "../helpers";
+
+type CardValueType = 'card' | 'player';
+type NumberData = {
+  card: number[];
+  player: number[];
+}
+
+export function getLineValues(line: string) {
+  const [_head, tail] = line.split(':');
+  const numberData: NumberData = {
+    card: [],
+    player: []
+  };
+  if (!tail) return numberData;
+  
+  const lineValues = tail.split(/\s+/);
+  let valueType: CardValueType = 'card';
+  while (lineValues.length) {
+    const val = lineValues.shift();
+    if (!val) continue;
+    if (val === '|') {
+      valueType = 'player';
+      continue;
+    }
+    numberData[valueType].push(Number(val));
+  }
+  return numberData;
+}
+
+export function getCardValue(numberData: NumberData) {
+  let value = 0;
+  for (const num of numberData.player) {
+    if (numberData.card.includes(num)) {
+      value = value * 2 || 1;
+    }
+  }
+  return value;
+}
+
+export function getSolutionA(file = './04/04.input.txt'): number {
+  const cardValues = loadLinesFromFile(file).map(getLineValues).map(getCardValue);
+  const total = cardValues.reduce((prev: number, curr: number) => {
+    return prev + curr;
+  }, 0);
+  return total;
+}
---
+++ src/lib/04/04.test.ts
@@ -0,0 +1,37 @@
+import { beforeAll, describe, expect, test } from 'vitest';
+import { loadLinesFromFile } from '../helpers';
+import { getSolutionA, getLineValues, getCardValue } from './04.a';
+
+describe('Day 4', () => {
+  let testFile = './04/04.input.test.txt';
+  let testInput: string[];
+  const testLineValues = {
+    card: [ 41, 48, 83, 86, 17 ],
+    player: [
+      83, 86,  6, 31,
+      17,  9, 48, 53
+    ]
+  };
+
+  beforeAll(() => {
+    testInput = loadLinesFromFile(testFile);
+  });
+
+  describe('Part A', () => {
+    test('parseLine', () => {
+      expect(getLineValues(testInput[0])).toMatchObject(testLineValues);
+    });
+
+    test('getCardValue', () => {
+      expect(getCardValue(testLineValues)).toBe(8);
+    });
+    
+    test('getSolution for test input', () => {
+      expect(getSolutionA(testFile)).toBe(13);
+    });
+
+    test('getSolution', () => {
+      console.log(getSolutionA());
+    });
+  });
+});
